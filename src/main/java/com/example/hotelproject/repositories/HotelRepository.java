32468ba82abd73510c0b3764291d6e7ef7f28524
Amir <cnooed@gmail.com>
2019-04-09 12:01:40 +0430

    implemented the HotelRepository To do Crud Operations

---
+++ src/main/java/com/example/hotelproject/repositories/HotelRepository.java
@@ -0,0 +1,23 @@
+package com.example.hotelproject.repositories;
+
+import com.example.hotelproject.model.Hotel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HotelRepository  extends MongoRepository<Hotel,String> {
+
+    Optional<Hotel> findById(String id);
+
+    @Override
+    <S extends Hotel> List<S> saveAll(Iterable<S> entities);
+
+    List<Hotel> findByPricePerNightLessThan(int maxPrice);
+
+    @Query(value = "{address.city:?0}")
+    List<Hotel> findByCity(String city);
+}
